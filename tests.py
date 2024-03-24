ace4d0a5cd9a2be481c960c939811c9eb0f31192
Егор Сидорович <ysydorovychy@gmail.com>
2024-03-24 15:54:46 +0300

    Фикс замечаний

--- tests.py
+++ tests.py
@@ -20,7 +20,7 @@ class TestBooksCollector:
 
         # проверяем, что добавилось именно две
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.books_genre.keys()) == 2
+        assert len(collector.get_books_genre()) == 2
 
     def test_set_book_genre_set_genre(self):
 
@@ -29,7 +29,7 @@ class TestBooksCollector:
         collector.add_new_book('Зловещие мертвецы 2')
         collector.set_book_genre('Зловещие мертвецы 2', 'Ужасы')
 
-        assert collector.books_genre['Зловещие мертвецы 2'] == 'Ужасы'
+        assert collector.get_books_genre()['Зловещие мертвецы 2'] == 'Ужасы'
 
     def test_get_book_genre_get_genre(self):
 
@@ -54,7 +54,7 @@ class TestBooksCollector:
 
     def test_get_books_genre_curent_status(self, list_book):
         for i in list_book.books_genre.keys():
-            assert i in list_book.books_genre
+            assert i in list_book.get_books_genre()
 
     def test_get_books_for_children_true(self, books_for_children):
         assert 'Зловещие мертвецы 2' not in books_for_children.get_books_for_children()
@@ -65,16 +65,16 @@ class TestBooksCollector:
         collector.add_new_book('Джей и молчаливый Боб')
         collector.add_book_in_favorites('Джей и молчаливый Боб')
 
-        assert 'Джей и молчаливый Боб' in collector.books_genre and collector.favorites
+        assert 'Джей и молчаливый Боб' in collector.get_books_genre() and collector.get_list_of_favorites_books()
 
-    @pytest.mark.parametrize('favorites', ['Очень страшное кино'])
-    def test_delete_book_from_favorites_delete_one_book(self, favorites):
+     #'Очень страшное кино'
+    def test_delete_book_from_favorites_delete_one_book(self):
         collector = BooksCollector()
 
-        collector.add_new_book(favorites)
-        collector.add_book_in_favorites(favorites)
-        collector.delete_book_from_favorites(favorites)
-        assert collector.favorites == []
+        collector.add_new_book('Очень страшное кино')
+        collector.add_book_in_favorites('Очень страшное кино')
+        collector.delete_book_from_favorites('Очень страшное кино')
+        assert collector.get_list_of_favorites_books() == []
 
     def test_get_list_of_favorites_books_true(self, favorit_book):
         collector = BooksCollector()
